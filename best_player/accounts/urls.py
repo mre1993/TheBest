4b5e4713ba2df7b44d6f9d796cc23a8118b29211
mre1993 <jordanmre1993@gmail.com>
2025-04-11 12:34:59 +0330

    first-edition

---
+++ best_player/accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from .views import UserRegistrationView, UserVerifyView, LoginView, logoutView
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("login/", LoginView.as_view(), name="login"),
+    path("register/", UserRegistrationView.as_view(), name="register"),
+    path("verify/", UserVerifyView.as_view(), name="verify"),
+    path("logout/", logoutView.as_view(), name="logout"),
+    # path("api/v1/", include("accounts.api.v1.urls")),
+]
